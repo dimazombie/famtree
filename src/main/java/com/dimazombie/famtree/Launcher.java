8a401854887d16e1676c63c7e30da7aaf6f0b17f
sushkevichd@gmail.com <sushkevichd@gmail.com>
2019-01-19 08:36:35 +0300

    added another way to init webappcontext, webapp dir moved to the root of a prj

--- src/main/java/com/dimazombie/famtree/Launcher.java
+++ src/main/java/com/dimazombie/famtree/Launcher.java
@@ -5,22 +5,37 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.net.URL;
 import java.util.Locale;
+import java.util.Objects;
 
 public class Launcher {
     public static final String WEBAPP_RESOURCES_LOCATION = "webapp";
 
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.ENGLISH);
-        Server server = new Server(8889);
-        WebAppContext context = new WebAppContext();
-        context.setContextPath("/");
-        context.setDescriptor(WEBAPP_RESOURCES_LOCATION + "/WEB-INF/web.xml");
-        URL webAppDir = Thread.currentThread().getContextClassLoader().getResource(WEBAPP_RESOURCES_LOCATION);
-        if (webAppDir == null) {
-            throw new RuntimeException(String.format("No %s directory was found into the JAR file", WEBAPP_RESOURCES_LOCATION));
+
+        int port = 8889;
+        if(args.length > 0) {
+            port = Integer.parseInt(args[0]);
         }
-        context.setResourceBase(webAppDir.toURI().toString());
-        context.setParentLoaderPriority(true);
+
+        Server server = new Server(port);
+        WebAppContext context = null;
+
+        String protocol = Launcher.class.getResource("").getProtocol();
+        if(Objects.equals(protocol, "jar")){
+            context = new WebAppContext();
+            context.setContextPath("/");
+            context.setDescriptor(WEBAPP_RESOURCES_LOCATION + "/WEB-INF/web.xml");
+
+            URL webAppDir = Thread.currentThread().getContextClassLoader().getResource(WEBAPP_RESOURCES_LOCATION);
+            if (webAppDir == null) {
+                throw new RuntimeException(String.format("No %s directory was found into the JAR file", WEBAPP_RESOURCES_LOCATION));
+            }
+            context.setResourceBase(webAppDir.toURI().toString());
+            context.setParentLoaderPriority(true);
+        } else {
+            context = new WebAppContext(WEBAPP_RESOURCES_LOCATION, "/");
+        }
 
         if (System.getProperty("os.name").toLowerCase().contains("windows")) {
             // Fix for Windows, so Jetty doesn't lock files
