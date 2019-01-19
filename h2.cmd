8f4aacad99d78dda8dd157670a042f5ca05007ce
sushkevichd@gmail.com <sushkevichd@gmail.com>
2019-01-19 08:42:44 +0300

    script to connect to db has been fixed

--- h2.cmd
+++ h2.cmd
@@ -1,3 +1,3 @@
 @REM This script will give you H2 database shel
 
-java -cp h2.jar org.h2.tools.Shell -url "jdbc:h2:./h2db" -user "sa" -password "sa"
+java -cp h2.jar org.h2.tools.Shell -url "jdbc:h2:./h2db;AUTO_SERVER=TRUE;DB_CLOSE_DELAY=-1" -user "sa" -password "sa"
